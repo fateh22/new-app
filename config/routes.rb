432e345ba3038eec4dea5d4792cae96b1c8d0806
Fateh Singh <33489085+fateh22@users.noreply.github.com>
2018-04-16 22:05:01 +0530

    contact template addded

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+
   resources :contact
   resources :about
-
   root 'home#index'
-
   get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
