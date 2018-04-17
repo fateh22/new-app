a9705abe8c9a7eb28db618ee2750b9bb7f71ee2f
Fateh Singh <33489085+fateh22@users.noreply.github.com>
2018-04-17 19:19:38 +0530

    pop sign in and up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :contact
-  resources :about
+  #devise_for :users
   root 'home#index'
-  get 'welcome/index'
-
+  resources :about
+  resources :contact
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  get 'welcome/index' #testing routes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
