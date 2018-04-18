d326f6d844b008bbf4002878c86b8e04dba5a7da
Fateh Singh <33489085+fateh22@users.noreply.github.com>
2018-04-18 23:28:26 +0530

    worked on contact page

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,23 @@
 class ContactController < ApplicationController
+
   def index
+    @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to  contact_index_path, notice: 'Thanks for Contact us' }
+      else
+        render 'index'
+      end
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit!
+  end
+
 end
